f1909282fd35b331045d2697ac60318e822dffed
jacobcjaffe <jacobcjaffe@gmail.com>
2023-10-15 18:57:07 -0700

    utilize the getNext method for basic dynamic scheduling

---
+++ Assignments/02/Report.md
@@ -0,0 +1,4 @@
+Inital Timing:
+Real: .659s
+User: 4.141s
+Sys: .005s
--- Assignments/02/run.sh
+++ Assignments/02/run.sh
@@ -6,11 +6,12 @@ make
 
 if [[ $1 -eq 0 ]]; then
 	ln -s Test/*.bin .
-	time ./lychrel
+	time ./lychrel > RESULTS
+	diff -s RESULTS Test/Output.txt
 elif [[$1 -eq 1]]; then
 	rm *.bin
 	ln -s Final/*.bin
-	time lychrel > RESULTS
+	time ./lychrel > RESULTS
 	diff -s RESULTS Final/Output.txt
 else
 	echo needs to be 0 or 1
