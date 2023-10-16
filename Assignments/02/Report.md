bd65a3fd6ccd6deec60d66b01ba33d7a43eaff91
jacobcjaffe <jacobcjaffe@gmail.com>
2023-10-15 20:13:41 -0700

    tried having one thread push to a queue of vectors using the getNext() method. Made it worse, possibly from copying data repeatedly

--- Assignments/02/Report.md
+++ Assignments/02/Report.md
@@ -1,12 +1,27 @@
 Commits
 1: created a bash file to quickly automate testing
 2: utilized the getNext function to retrieve the next number. 
+3: created two iterators. Have half of the threads getNext from the front, and
+    the other half retrieving from the back of the numbers.
+4: Tried creating a queue, but timing is worse. I think it may have to do with copying the
+    data repeatedly
 Inital Timing:
 Real: .659s
 User: 4.141s
 Sys: .005s
 
-Commit 1:
-real: .633s
+Commit 2:
+Real: .633s
 User: 4.038s
 Sys: .010s
+
+Commit 3:
+Real: .486s
+User: 4.672s
+Sys .002s
+
+Commit 4:
+Real: .534s
+User: 4.268s
+Sys: .007s
+
--- Assignments/02/lychrel.cpp
+++ Assignments/02/lychrel.cpp
@@ -17,6 +17,7 @@
 #include <vector>
 #include <thread>
 #include <mutex>
+#include <queue>
 
 
 #include "LychrelData.h"
@@ -50,47 +51,28 @@ int main() {
     std::barrier barrier{MaxThreads};
     //is how the threads communicate with eachother to tell when the variables they share are allowed to be changed by them.
     std::mutex mutex;
+	std::mutex queueMutex;
 
+	std::atomic<bool> _finished = false;
+	std::queue<std::vector<Number>> vecQueue;
     // here is where we can parallelize the code
 	int LastId = MaxThreads - 1;
 
 	// ok so first we need to loop through he threads at each pass
 	for (int id = 0; id < MaxThreads; ++id){
-		if (id < 5) {
+		// have two threads continuously grabbing data
+		if (id == 0) {
 			std::thread th{[&, id]() {
-				Number number;
-				size_t iter = 0;
-				while(data.getNext(number)) {
-					iter = 0;
-					Number n = number;
-					while (!n.is_palindrome() && ++iter < MaxIterations) {
-						Number sum(n.size());
-						Number r = n.reverse();
-						auto rd = n.begin();
-						bool carry = false;
-						std::transform(n.rbegin(), n.rend(), sum.rbegin(), 
-							[&](auto d) {
-								auto v = d + *rd++ + carry;
-								carry = v > 9;
-								if (carry) { v -= 10; }
-								return v;
-							}
-						);
-						if (carry) { sum.push_front(1); }
-						n = sum;
-					}
+				std::vector<Number> buffer(4);
+				while (data.getNext(4, buffer)){
+					std::vector<Number> vect2;
+					copy(buffer.begin(), buffer.end(), back_inserter(vect2));
 					{
-						std::lock_guard lock{mutex}; 
-						if (!(iter < maxIter || iter == MaxIterations)){
-							Record record{number, n};
-							if (iter > maxIter) {
-								records.clear();
-								maxIter = iter;
-							}
-							records.push_back(record);
-						}
+						std::lock_guard lock{queueMutex};
+						vecQueue.push(vect2);
 					}
 				}
+				_finished = true;
 				barrier.arrive_and_wait();
 			}};
 			(id < LastId) ? th.detach() : th.join();
@@ -99,34 +81,56 @@ int main() {
 			std::thread th{[&, id]() {
 				Number number;
 				size_t iter = 0;
-				while(data.getNext(number)) {
-					iter = 0;
-					Number n = number;
-					while (!n.is_palindrome() && ++iter < MaxIterations) {
-						Number sum(n.size());
-						Number r = n.reverse();
-						auto rd = n.begin();
-						bool carry = false;
-						std::transform(n.rbegin(), n.rend(), sum.rbegin(), 
-							[&](auto d) {
-								auto v = d + *rd++ + carry;
-								carry = v > 9;
-								if (carry) { v -= 10; }
-								return v;
-							}
-						);
-						if (carry) { sum.push_front(1); }
-						n = sum;
-					}
+				std::vector<Number> nums;
+				while(true) {
+					// check to see if thread can pull from queue
+					nums.clear();
 					{
-						std::lock_guard lock{mutex}; 
-						if (!(iter < maxIter || iter == MaxIterations)){
-							Record record{number, n};
-							if (iter > maxIter) {
-								records.clear();
-								maxIter = iter;
+						std::lock_guard lock{queueMutex};
+						if (vecQueue.empty() && _finished) {
+							break;
+						}					
+						else if (!_finished) {
+							// need to wait for the queue to be filled
+							std::this_thread::yield();
+							continue;
+						}
+						else {
+							copy(vecQueue.front().begin(), vecQueue.front().end(), back_inserter(nums));
+							vecQueue.pop();
+						}
+					}
+					
+					for (int i = 0; i < nums.size(); i++) {
+						number = nums[i];
+						iter = 0;
+						Number n = number;
+						while (!n.is_palindrome() && ++iter < MaxIterations) {
+							Number sum(n.size());
+							Number r = n.reverse();
+							auto rd = n.begin();
+							bool carry = false;
+							std::transform(n.rbegin(), n.rend(), sum.rbegin(), 
+								[&](auto d) {
+									auto v = d + *rd++ + carry;
+									carry = v > 9;
+									if (carry) { v -= 10; }
+									return v;
+								}
+							);
+							if (carry) { sum.push_front(1); }
+							n = sum;
+						}
+						{
+							std::lock_guard lock{mutex}; 
+							if (!(iter < maxIter || iter == MaxIterations)){
+								Record record{number, n};
+								if (iter > maxIter) {
+									records.clear();
+									maxIter = iter;
+								}
+								records.push_back(record);
 							}
-							records.push_back(record);
 						}
 					}
 				}
@@ -134,70 +138,7 @@ int main() {
 			}};
 			(id < LastId) ? th.detach() : th.join();
 		}
-
-        
-		/*
-        // need to divide the the data into chunks that will be given to the threads.
-        size_t chunkSize = int(data.size() / MaxThreads + 1);
-        
-        std::thread t{[&,id]() {
-            auto start = id * chunkSize;
-            auto end = std::min(data.size(), start + chunkSize);
-            for(auto i = start; i < end; ++i) {
-                size_t iter = 0;
-                Number number = data[i];
-                Number n = number;
-                while (!n.is_palindrome() && ++iter < MaxIterations) {
-					Number sum(n.size());   // Value used to store current sum of digits
-					Number r = n.reverse(); // reverse the digits of the value
-					auto rd = n.begin(); 
-					bool carry = false;  // flag to indicate if we had a carry
-					std::transform(n.rbegin(), n.rend(), sum.rbegin(), 
-						[&](auto d) {
-							auto v = d + *rd++ + carry;
-							carry = v > 9;
-							if (carry) { v -= 10; }
-							return v;
-						}
-					);
-					if (carry) { sum.push_front(1); }
-					n = sum;
-				}
-				{
-					std::lock_guard lock{mutex}; 
-					if (!(iter < maxIter || iter == MaxIterations)){
-						Record record{number, n};
-						if (iter > maxIter) {
-							records.clear();
-							maxIter = iter;
-						}
-						records.push_back(record);
-					}
-				}
-			}
-        barrier.arrive_and_wait();
-        }};
-		*/
-        //(id < LastId) ? t.detach() : t.join();
 	}
-        // Update our records.  First, determine if we have a new
-        //   maximum number of iterations that isn't the control limit
-        //   (MaxIterations) for a particular number.  If we're less
-        //   tha the current maximum (maxIter) or we've exceeded the number
-        //   of permissible iterations, ignore the current result and move
-        //   onto the next number.
-        //if (iter < maxIter || iter == MaxIterations) { continue; }
-
-        // Otherwise update our records, which possibly means discarding
-        //   our current maximum and rebuilding our records list.
-        //Record record{number, n};
-        //if (iter > maxIter) {
-        //    records.clear();
-        //    maxIter = iter;
-       // }
-
-        //records.push_back(record);
-    
     // Output our final results
     std::cout << "\nmaximum number of iterations = " << maxIter << "\n";
     for (auto& [number, palindrome] : records) {
